7ef59358897782e4bf14c10d1e62d426de5c889c
Mykhailo <kmetiuk@icloud.com>
2026-01-15 22:00:00 -0800

    more tests added to improve coverage

--- tests/unit/test_grade.py
+++ tests/unit/test_grade.py
@@ -19,3 +19,16 @@ def test_quiz_math_case():
     expected = pytest.approx(57.142857, abs=1e-3)
     for k in ["lab2", "lab3", "lab4", "quiz1", "quiz2"]:
         assert result[k] == expected
+def test_project_milestone_math_case():
+    result = needed_to_pass("project", {"Milestone1": 80})
+    expected = pytest.approx(55.0, abs=1e-3)
+
+    for k in [
+        "Milestone2", "Milestone3", "Milestone4",
+        "IndividualAssignment1", "IndividualAssignment2",
+        "IndividualAssignment3", "IndividualAssignment4"
+    ]:
+        assert result[k] == expected
+def test_impossible_to_pass():
+    with pytest.raises(ValueError):
+        needed_to_pass("quiz", {"lab1": 5, "lab2": 10, "quiz1": 5, "lab3": 10})
