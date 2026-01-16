197044db967c6f348b4f242ee8b6cecd1ab76c7c
iangault <142769857+iangault@users.noreply.github.com>
2026-01-16 10:09:00 -0800

    simplifying status docstring to just be status and not absense as well

--- src/ubc_mds_helper/status.py
+++ src/ubc_mds_helper/status.py
@@ -1,38 +1,36 @@
 """
-A date and calendar-based Python module that describes position in the UBC MDS program and absence-related information.
+A date and calendar-based Python module that describes position in the UBC MDS program.
 
 Author: Ian Gault
 
 Date: January 9, 2026
 """
 
+from datetime import datetime, date
+from config import PROGRAM_CONFIG_2025_2026
+from helper_functions import normalize_date
+
+
 def status(
     config,
-    date=None,
-    days_missed=None
+    date_input=None
 ):
     """
 
     Return program status for a given date.
-    
-    If `days_missed` is a used argument, then program status will also return absence-related information.
 
     Parameters
     ----------
     config : from the config.py file
         Contains program date information, block structure, and break periods.
-    date : str or Python datetime object, optional
+    date_input : str or Python datetime object, optional
         Date at which program status is based upon.
         If None, defaults to today's date.
-    days_missed: default is None, optional
-        Number of days missed from the 'date' argument.
-        If argument is provided, information about absence also returned.
 
     Returns
     -------
     Dict
         Dictionary will be returned descibing academic position based on date.
-        If 'days_missed' is included, then absence-related information will also be included.
         Dictionary is printed out in the console for viewing
 
     Examples
@@ -52,17 +50,27 @@ def status(
         "next_break_name": None
     }
 
-    >>> status(config, date="Mar 16, 2026", days_missed=5)
+    """
 
-    Displays program status from entered date.
-    In  addition to the above results in `result`, the following keys will be included:
+    # if the date passed in are None, retrieve them from PROGRAM_CONFIG_2025_2026 in config.py
+    if date_input is None:
+        date_input = date.today()
 
-    result = {
-        "days_missed": int,
-        "return_date": date,
-        "missed_blocks": str,
-        "missed_week_in_block": int
+    # normalize the dates that are passed in: if argument is a datetime.date, return date; if string, try to convert it to an accepted datetime format or throw an error
+    date_input = normalize_date(date_input)
+
+    return {
+        "date": date_input,
+        "day_of_week": date_input.strftime("%A"),
+        "block": None,
+        "week_in_block": None,
+        "during_break": False,
+        "break_name": None,
+        "days_until_next_break": None,
+        "next_break_name": None,
     }
 
-    """
-    pass 
+output = status(PROGRAM_CONFIG_2025_2026, "Feb 8, 2026")
+
+for key, value in output.items():
+    print(f"{key}: {value}")
